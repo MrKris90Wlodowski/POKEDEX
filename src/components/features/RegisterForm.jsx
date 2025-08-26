0f29ab86e0b67f2293cdf539527a6f2cebc020fe
Kris1990_PL <MRK1990@wp.pl>
2025-08-26 10:14:34 +0200

    feat(RegisterForm): improve and add schema validation to registration form

--- src/components/features/RegisterForm.jsx
+++ src/components/features/RegisterForm.jsx
@@ -1,26 +1,70 @@
-import Button from "../shared/Button"
-import Input from "../shared/Input"
-import Wrapper from "../shared/Wrapper"
-import Form from "../shared/Form"
+import Button from "../shared/Button";
+import Input from "../shared/Input";
+import Wrapper from "../shared/Wrapper";
+import Form from "../shared/Form";
 
-import { useForm } from "react-hook-form"
-import * as z from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-const schema = z.object({});
+const schema = z.object({
+    name: z
+    .string()
+    .nonempty("This field is required")
+    .min(3,"Name must contains minimum 3 signs"),
+    email: z
+    .string()
+    .nonempty("This field is required")
+    .email("This field must contains correct address email"),
+    password: z
+    .string()
+    .nonempty("This field is required")
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8}$/,"This field must contains at least one upper, lower letter and digit all minimum 8 signs"),
+    confirmPassword: z
+    .string()
+}).refine((dataPassword) => dataPassword.password === dataPassword.confirmPassword, {
+    message: "Confirm password must be match to password",
+    path: ["confirmPassword"]
+});
 
 const RegisterForm = () => {
-    return (
-        <Wrapper>
-            <Form>
-                <Input id="name" name="name" type="text">NAME:</Input>
-                <Input id="email" name="email" type="email">EMAIL:</Input>
-                <Input id="password" name="password" type="password">PASSWORD:</Input>
-                <Input id="confirmPassword" name="confirmPassword" type="password">CONFIRM PASSWORD:</Input>
-                <Button type="submit">REGISTER</Button>
-            </Form>
-        </Wrapper>
-    )
+
+  const { register, reset, handleSubmit } = useForm({resolver: zodResolver(schema)});
+
+  const dataRegister = (formValue) => {
+    console.log(formValue);
+    reset();
 }
 
-export default RegisterForm
+  return (
+    <Wrapper>
+      <Form onSubmit={handleSubmit(dataRegister)}>
+        <Input id="name" name="name" type="text" register={register}>
+          NAME:
+        </Input>
+        <Input id="email" name="email" type="email" register={register}>
+          EMAIL:
+        </Input>
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          register={register}
+        >
+          PASSWORD:
+        </Input>
+        <Input
+          id="confirmPassword"
+          name="confirmPassword"
+          type="password"
+          register={register}
+        >
+          CONFIRM PASSWORD:
+        </Input>
+        <Button type="submit">REGISTER</Button>
+      </Form>
+    </Wrapper>
+  );
+};
+
+export default RegisterForm;
--- src/components/shared/Input.jsx
+++ src/components/shared/Input.jsx
@@ -2,7 +2,7 @@ const Input = ({ id, type, name, children, register, errors }) => {
   return (
     <div>
       <label htmlFor={name}>{children}</label>
-      <input id={id} type={type} name={name}/>
+      <input id={id} type={type} name={name} {...(register ? register(name) : {})}/>
     </div>
   );
 };
