b669450d9d9bd68341dc8c3c3aac62196b9f0804
Kris1990_PL <MRK1990@wp.pl>
2025-08-26 11:02:50 +0200

    feat(RegisterForm): add error messages to registration form

--- src/components/features/RegisterForm.jsx
+++ src/components/features/RegisterForm.jsx
@@ -7,42 +7,65 @@ import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-const schema = z.object({
+const schema = z
+  .object({
     name: z
-    .string()
-    .nonempty("This field is required")
-    .min(3,"Name must contains minimum 3 signs"),
+      .string()
+      .nonempty("This field is required")
+      .min(3, "Name must contains minimum 3 signs"),
     email: z
-    .string()
-    .nonempty("This field is required")
-    .email("This field must contains correct address email"),
+      .string()
+      .nonempty("This field is required")
+      .email("This field must contains correct address email"),
     password: z
-    .string()
-    .nonempty("This field is required")
-    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8}$/,"This field must contains at least one upper, lower letter and digit all minimum 8 signs"),
-    confirmPassword: z
-    .string()
-}).refine((dataPassword) => dataPassword.password === dataPassword.confirmPassword, {
-    message: "Confirm password must be match to password",
-    path: ["confirmPassword"]
-});
+      .string()
+      .nonempty("This field is required")
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8}$/,
+        "This field must contains at least one upper, lower letter and digit all minimum 8 signs"
+      ),
+    confirmPassword: z.string(),
+  })
+  .refine(
+    (dataPassword) => dataPassword.password === dataPassword.confirmPassword,
+    {
+      message: "Confirm password must be match to password",
+      path: ["confirmPassword"],
+    }
+  );
 
 const RegisterForm = () => {
-
-  const { register, reset, handleSubmit } = useForm({resolver: zodResolver(schema)});
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
 
   const dataRegister = (formValue) => {
     console.log(formValue);
     reset();
-}
+  };
 
   return (
     <Wrapper>
       <Form onSubmit={handleSubmit(dataRegister)}>
-        <Input id="name" name="name" type="text" register={register}>
+        <Input
+          id="name"
+          name="name"
+          type="text"
+          register={register}
+          errors={errors}
+        >
           NAME:
         </Input>
-        <Input id="email" name="email" type="email" register={register}>
+        <Input
+          id="email"
+          name="email"
+          type="email"
+          register={register}
+          errors={errors}
+        >
           EMAIL:
         </Input>
         <Input
@@ -50,6 +73,7 @@ const RegisterForm = () => {
           name="password"
           type="password"
           register={register}
+          errors={errors}
         >
           PASSWORD:
         </Input>
@@ -58,6 +82,7 @@ const RegisterForm = () => {
           name="confirmPassword"
           type="password"
           register={register}
+          errors={errors}
         >
           CONFIRM PASSWORD:
         </Input>
--- src/components/shared/Input.jsx
+++ src/components/shared/Input.jsx
@@ -3,6 +3,7 @@ const Input = ({ id, type, name, children, register, errors }) => {
     <div>
       <label htmlFor={name}>{children}</label>
       <input id={id} type={type} name={name} {...(register ? register(name) : {})}/>
+      {errors?.[id] && <p style={{color: "red"}}>{errors[id].message}</p>}
     </div>
   );
 };
