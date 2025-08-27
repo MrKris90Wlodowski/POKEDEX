041347a7edf872a59b11c8afaa701e6d697e8c34
Kris1990_PL <MRK1990@wp.pl>
2025-08-27 15:26:11 +0200

    refactor(Pokedex): restructure App and main, move RouterProvider to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { createBrowserRouter, BrowserRouter, RouterProvider } from 'react-router-dom'
 import Arena from "./components/subpages/Arena"
 import Edition from "./components/subpages/Edition"
 import Favourite from "./components/subpages/Favourite "
@@ -5,20 +6,27 @@ import Home from "./components/subpages/Home"
 import Login from "./components/subpages/Login"
 import Ranking from "./components/subpages/Ranking"
 import Registration from "./components/subpages/Registration"
+import RootLayout from './layouts/RootLayout.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:"",
+    element: <RootLayout/>,
+    children: [
+      {index: true, element: <Home/>},
+      {path: "/registration", element: <Registration/>},
+      {path: "/login", element: <Login/>},
+      {path: "/favourite", element: <Favourite/>},
+      {path: "/arena", element: <Arena/>},
+      {path: "/ranking", element: <Ranking/>},
+      {path: "/edition", element: <Edition/>},
+    ]
+  }
+])
 
 const App = () => {
-
   return (
-    <>
-     <h1>HELLO IN POKEDEX</h1>
-     <Home/>
-     <Registration/>
-     <Login/>
-     <Favourite/>
-     <Arena/>
-     <Ranking/>
-     <Edition/>
-    </>
+   <RouterProvider router={router}/>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, BrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-// import App from './App.jsx'
+import App from './App.jsx'
 
-import Arena from "./components/subpages/Arena"
-import Edition from "./components/subpages/Edition"
-import Favourite from "./components/subpages/Favourite "
-import Home from "./components/subpages/Home"
-import Login from "./components/subpages/Login"
-import Ranking from "./components/subpages/Ranking"
-import Registration from "./components/subpages/Registration"
-import RootLayout from './layouts/RootLayout.jsx'
 
-const router = createBrowserRouter([
-  {
-    path:"",
-    element: <RootLayout/>,
-    children: [
-      {index: true, element: <Home/>},
-      {path: "/registration", element: <Registration/>},
-      {path: "/login", element: <Login/>},
-      {path: "/favourite", element: <Favourite/>},
-      {path: "/arena", element: <Arena/>},
-      {path: "/ranking", element: <Ranking/>},
-      {path: "/edition", element: <Edition/>},
-    ]
-  }
-])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <App/>
   </StrictMode>,
 )
