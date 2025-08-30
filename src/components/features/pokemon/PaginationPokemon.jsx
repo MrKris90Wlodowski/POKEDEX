0c3217f16b549484769ca7576f8f61469dd23ed8
Kris1990_PL <MRK1990@wp.pl>
2025-08-30 18:05:21 +0200

    fix(PaginationPokemon): add edge navigation (first & last page) with block

--- src/components/features/pokemon/PaginationPokemon.jsx
+++ src/components/features/pokemon/PaginationPokemon.jsx
@@ -3,44 +3,57 @@ import Wrapper from "../../shared/Wrapper";
 import Button from "../../shared/Button";
 import Text from "../../shared/Text";
 
-const PaginationPokemon = ({recordLength}) => {
+const PaginationPokemon = ({ recordLength }) => {
   const [currentPage, setCurrentPage] = useState(1);
-//   const [totalRecords, setTotalRecords] = useState(recordLength);
-//   const [disabled, setDisable] = useState(false);
+  const [disabledPrev, setDisabledPrev] = useState(false);
+  const [disabledNext, setDisabledNext] = useState(false);
 
-const lastPage = Math.ceil(recordLength/15);
-console.log(lastPage);
-console.log(recordLength);
+  const lastPage = Math.ceil(recordLength / 15);
+  console.log(lastPage);
+  console.log(recordLength);
 
-// const maxPage = (recordLength) => {
-// }
- 
   const handleFirstPage = () => {
-    setCurrentPage(prev => 1)
-  }
-  const handleNextPage = () => {
-    setCurrentPage((prev) => prev + 1);
+    setCurrentPage((prev) => 1);
+    setDisabledNext(false);
   };
   const handlePrevPage = () => {
     setCurrentPage((prev) => prev - 1);
+    setDisabledNext(false);
+    if (currentPage <= 1) {
+      setCurrentPage((prev) => 1);
+      setDisabledPrev(true);
+    }
+  };
+  const handleNextPage = () => {
+    setCurrentPage((prev) => prev + 1);
+    setDisabledPrev(false);
+    if (currentPage >= lastPage) {
+      setCurrentPage((prev) => lastPage);
+      setDisabledNext(true);
+    }
   };
   const HandleLastPage = () => {
-    setCurrentPage(prev => lastPage)
-  }
+    setCurrentPage((prev) => lastPage);
+    setDisabledPrev(false);
+  };
 
   return (
     <Wrapper className="flex gap-4 my-7">
-      <Button onClick={() => handleFirstPage()}>FIRST</Button>
-      <Button disabled={currentPage === 1} onClick={() => handlePrevPage()}>
+      <Button disabled={disabledPrev} onClick={() => handleFirstPage()}>
+        FIRST
+      </Button>
+      <Button disabled={disabledPrev} onClick={() => handlePrevPage()}>
         PREV
       </Button>
       <Wrapper className="flex items-center justify-center border-2 border-white rounded-lg w-11">
-        <Text>{currentPage}</Text> 
+        <Text>{currentPage}</Text>
       </Wrapper>
-      <Button disabled={currentPage === lastPage} onClick={() => handleNextPage()}>
+      <Button disabled={disabledNext} onClick={() => handleNextPage()}>
         NEXT
       </Button>
-      <Button onClick={() => HandleLastPage()}>LAST</Button>
+      <Button disabled={disabledNext} onClick={() => HandleLastPage()}>
+        LAST
+      </Button>
     </Wrapper>
   );
 };
--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,7 +1,29 @@
-const Button = ({type="button", children, onClick, disabled}) => {
-    return (
-        <button disabled={disabled} type={type} onClick={onClick}>{children}</button>
+import clsx from "clsx"
+
+const Button = ({
+  type = "button",
+  children,
+  onClick,
+  disabled,
+}) => {
+
+    const buttonClass = clsx(
+        "px-4 py-2 text-white rounded-lg cursor-pointer ", 
+        disabled 
+      ? "bg-gray-500"
+      : "bg-blue-500 hover:bg-blue-700 transition duration-300 ease-in-out"
     )
-}
+
+  return (
+    <button
+      disabled={disabled}
+      type={type}
+      className={buttonClass}   
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
