dbd4bef4f4d55977cd4009ac42bb99d6d86d5308
Kris1990_PL <MRK1990@wp.pl>
2025-08-31 00:46:41 +0200

    refactor(Home, PaginationPokemon): move pagination state to Home

--- src/components/features/pokemon/PaginationPokemon.jsx
+++ src/components/features/pokemon/PaginationPokemon.jsx
@@ -1,57 +1,25 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Wrapper from "../../shared/Wrapper";
 import Button from "../../shared/Button";
 import Text from "../../shared/Text";
 
-const PaginationPokemon = ({ recordLength }) => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [disabledPrev, setDisabledPrev] = useState(false);
-  const [disabledNext, setDisabledNext] = useState(false);
-
-  const lastPage = Math.ceil(recordLength / 15);
-  console.log(lastPage);
-  console.log(recordLength);
-
-  const handleFirstPage = () => {
-    setCurrentPage((prev) => 1);
-    setDisabledNext(false);
-  };
-  const handlePrevPage = () => {
-    setCurrentPage((prev) => prev - 1);
-    setDisabledNext(false);
-    if (currentPage <= 1) {
-      setCurrentPage((prev) => 1);
-      setDisabledPrev(true);
-    }
-  };
-  const handleNextPage = () => {
-    setCurrentPage((prev) => prev + 1);
-    setDisabledPrev(false);
-    if (currentPage >= lastPage) {
-      setCurrentPage((prev) => lastPage);
-      setDisabledNext(true);
-    }
-  };
-  const HandleLastPage = () => {
-    setCurrentPage((prev) => lastPage);
-    setDisabledPrev(false);
-  };
+const PaginationPokemon = ({ currentPage, lastPage, onChange }) => {
 
   return (
     <Wrapper className="flex gap-4 my-7">
-      <Button disabled={disabledPrev} onClick={() => handleFirstPage()}>
+      <Button disabled={currentPage === 1} onClick={() => onChange("FIRST")}>
         FIRST
       </Button>
-      <Button disabled={disabledPrev} onClick={() => handlePrevPage()}>
+      <Button disabled={currentPage === 1} onClick={() => onChange("PREV")}>
         PREV
       </Button>
       <Wrapper className="flex items-center justify-center border-2 border-white rounded-lg w-11">
         <Text>{currentPage}</Text>
       </Wrapper>
-      <Button disabled={disabledNext} onClick={() => handleNextPage()}>
+      <Button disabled={currentPage === lastPage} onClick={() => onChange("NEXT")}>
         NEXT
       </Button>
-      <Button disabled={disabledNext} onClick={() => HandleLastPage()}>
+      <Button disabled={currentPage === lastPage} onClick={() => onChange("LAST")}>
         LAST
       </Button>
     </Wrapper>
--- src/components/subpages/Home.jsx
+++ src/components/subpages/Home.jsx
@@ -6,8 +6,8 @@ import { hash } from "zod";
 import PaginationPokemon from "../features/pokemon/PaginationPokemon";
 
 const Home = () => {
+  // State and handle SearchBarPokemon
   const [searchValue, setSearchValue] = useState("");
-  const [recordsLength, setRecordsLength] = useState(0);
 
   const handleRecordLength = (recLength) => {
     setRecordsLength(recLength)
@@ -15,12 +15,24 @@ const Home = () => {
   const handleSearch = (value) => {
     setSearchValue(value)
   }
+  
+  // State and handle PaginationPokemon
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recordsLength, setRecordsLength] = useState(0);
+  const lastPage = Math.ceil(recordsLength / 15);
+
+  const handleChangePage = (action) => {
+    if (action === "FIRST") {setCurrentPage(1)} 
+    if (action === "PREV") {setCurrentPage(prev => prev - 1)}
+    if (action === "NEXT") {setCurrentPage(prev => prev + 1)}
+    if (action === "LAST") {setCurrentPage(prev => lastPage)}
+  } 
 
   return (
     <Wrapper>
       <SearchBarPokemon onSearch={handleSearch} value={searchValue}/>
       <PokemonContainer className="grid grid-cols-5 gap-6 p-6" value={searchValue} onRecordLength={handleRecordLength}/>
-      <PaginationPokemon recordLength={recordsLength}/>
+      <PaginationPokemon currentPage={currentPage} lastPage={lastPage} onChange={handleChangePage}/>
     </Wrapper>
   );
 };
