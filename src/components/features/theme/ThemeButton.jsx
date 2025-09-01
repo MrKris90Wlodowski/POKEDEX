c09bf0ae050f038d166ff14af0b010f7a1a9fa39
Kris1990_PL <MRK1990@wp.pl>
2025-09-02 01:59:34 +0200

    style(Navbar): add lucide icons for theme toggle and polish links UI

--- src/components/features/theme/ThemeButton.jsx
+++ src/components/features/theme/ThemeButton.jsx
@@ -1,14 +1,21 @@
 import Button from "../../shared/Button";
-import Wrapper from "../../shared/Wrapper"
+import Wrapper from "../../shared/Wrapper";
 import useTheme from "../../../hooks/useTheme";
+import { Sun } from "lucide-react";
+import { Moon } from "lucide-react";
 
 const ThemeButton = () => {
-    const { theme, toggleTheme } = useTheme()
-    return (
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <Wrapper>
+      <Button variant={theme} onClick={toggleTheme}>
         <Wrapper>
-            <Button variant={theme} onClick={toggleTheme}>THEME</Button>
+          <Sun />
+          <Moon />
         </Wrapper>
-    )
-}
+      </Button>
+    </Wrapper>
+  );
+};
 
 export default ThemeButton;
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,29 +1,63 @@
 import { Link, Outlet } from "react-router-dom";
 import Wrapper from "../components/shared/Wrapper";
-import Image from "../components/shared/Image"
+import Image from "../components/shared/Image";
 import Footer from "../components/subpages/Footer";
 import ThemeButton from "../components/features/theme/ThemeButton";
 import useTheme from "../hooks/useTheme";
-import logoPokemon from "../icons/pngegg.png"
+import logoPokemon from "../icons/pngegg.png";
 
 const RootLayout = () => {
-  const { theme } = useTheme()
+  const { theme } = useTheme();
   return (
     <Wrapper className="flex flex-col min-h-screen" variant={theme}>
       <nav className="flex gap-4 p-8 bg-blue-500">
-        <Link to=""><Wrapper className="w-48 h-16"><Image src={logoPokemon} alt="logo Pokemon"/></Wrapper></Link>
-        <Link to="/registration" className="text-[var(--yellow)] font-black text-2xl border-4 p-3 rounded-2xl">REGISTRATION</Link>
-        <Link to="/login" className="text-[var(--yellow)] font-black text-2xl border-4 p-3 rounded-2xl">LOGIN</Link>
-        <Link to="/favourite" className="text-[var(--yellow)] font-black text-2xl border-4 p-3 rounded-2xl">FAVOURITE</Link>
-        <Link to="/arena" className="text-[var(--yellow)] font-black text-2xl border-4 p-3 rounded-2xl">ARENA</Link>
-        <Link to="/ranking" className="text-[var(--yellow)] font-black text-2xl border-4 p-3 rounded-2xl">RANKING</Link>
-        <Link to="/edition" className="text-[var(--yellow)] font-black text-2xl border-4 p-3 rounded-2xl">EDITION</Link>
-        <ThemeButton/>
+        <Link to="">
+          <Wrapper className="w-48 h-16">
+            <Image src={logoPokemon} alt="logo Pokemon" />
+          </Wrapper>
+        </Link>
+        <Link
+          to="/registration"
+          className="text-[var(--yellow)] font-black text-2xl border-4 p-3 rounded-2xl hover:bg-blue-700 transition duration-300 ease-in-out"
+        >
+          REGISTRATION
+        </Link>
+        <Link
+          to="/login"
+          className="text-[var(--yellow)] font-black text-2xl border-4 p-3 rounded-2xl hover:bg-blue-700 transition duration-300 ease-in-out"
+        >
+          LOGIN
+        </Link>
+        <Link
+          to="/favourite"
+          className="text-[var(--yellow)] font-black text-2xl border-4 p-3 rounded-2xl hover:bg-blue-700 transition duration-300 ease-in-out"
+        >
+          FAVOURITE
+        </Link>
+        <Link
+          to="/arena"
+          className="text-[var(--yellow)] font-black text-2xl border-4 p-3 rounded-2xl hover:bg-blue-700 transition duration-300 ease-in-out"
+        >
+          ARENA
+        </Link>
+        <Link
+          to="/ranking"
+          className="text-[var(--yellow)] font-black text-2xl border-4 p-3 rounded-2xl hover:bg-blue-700 transition duration-300 ease-in-out"
+        >
+          RANKING
+        </Link>
+        <Link
+          to="/edition"
+          className="text-[var(--yellow)] font-black text-2xl border-4 p-3 rounded-2xl hover:bg-blue-700 transition duration-300 ease-in-out"
+        >
+          EDITION
+        </Link>
+        <ThemeButton />
       </nav>
       <Wrapper className="flex items-center justify-center flex-grow h-auto">
         <Outlet />
       </Wrapper>
-      <Footer/>
+      <Footer />
     </Wrapper>
   );
 };
