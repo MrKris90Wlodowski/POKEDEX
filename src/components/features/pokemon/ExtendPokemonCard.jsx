2c5f5807602b92052f5ad2e2d9235a00c6f7958b
Kris1990_PL <MRK1990@wp.pl>
2025-09-04 14:09:38 +0200

    style(ExtendPokemonCard): improve frame and layout ExtendPokemonCard

--- src/components/features/pokemon/ExtendPokemonCard.jsx
+++ src/components/features/pokemon/ExtendPokemonCard.jsx
@@ -3,21 +3,35 @@ import Image from "../../shared/Image";
 import Text from "../../shared/Text";
 import { useParams } from "react-router-dom";
 import useTheme from "../../../hooks/useTheme";
+import useAuth from "../../../hooks/useAuth";
+import { Heart } from "lucide-react";
+import { Sword } from "lucide-react";
 
 const ExtendPokemonCard = () => {
   const { pokemon } = useParams();
   const { theme } = useTheme();
+  const { log } = useAuth();
   console.log(pokemon);
 
   return (
-    <Wrapper className="flex gap-8 p-8 border-4 w-5xl rounded-4xl" variantBackground={ theme }>
-      <Wrapper className="border-4 rounded-4xl h-96 w-96" variant={ theme }>
+    <Wrapper
+      className="flex gap-8 p-8 border-4 w-5xl rounded-4xl"
+      variantBackground={theme}
+    >
+      <Wrapper className="border-4 rounded-4xl h-96 w-96" variant={theme}>
         <Image />
       </Wrapper>
-      <Wrapper>
-        <Wrapper></Wrapper>
-        <Wrapper> 
-          <Text>POKE</Text>
+      <Wrapper className="flex flex-col justify-between">
+        <Wrapper className="flex" variantLog={log}>
+          <Text className="text-2xl font-black text-green-700">W:</Text>
+          <Text className="text-2xl font-black text-red-700">L:</Text>
+          <Sword />
+          <Heart />
+        </Wrapper>
+        <Wrapper>
+          <Text tag="h3" className="my-2 font-black uppercase">
+            POKE
+          </Text>
           <Text strong={"HEIGHT:"}></Text>
           <Text strong={"WEIGHT:"}></Text>
           <Text strong={"BASE EXP:"}></Text>
--- src/components/shared/Wrapper.jsx
+++ src/components/shared/Wrapper.jsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 
-const Wrapper = ({ children, style, className, variant, variantBackground }) => {
+const Wrapper = ({ children, style, className, variant, variantBackground, variantLog }) => {
   const variantsPrimary = {
     light: "bg-[var(--white)] text-[var(--black)]",
     dark: "bg-[var(--black)] text-[var(--white)]",
@@ -10,9 +10,15 @@ const Wrapper = ({ children, style, className, variant, variantBackground }) => 
     dark: "bg-[var(--lightBlack)]",
   };
 
+  const activeUser = {
+      login: "block",
+      logout: "hidden"
+    }
+
   const wrapperClass = clsx(
         variantsPrimary[variant],
         variantsSecondary[variantBackground],
+        activeUser[variantLog],
         className
     )
 
