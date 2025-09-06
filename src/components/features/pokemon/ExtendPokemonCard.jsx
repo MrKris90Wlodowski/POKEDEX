afaec26a5f0faeacc87db5215e0372f0ceca0aca
Kris1990_PL <MRK1990@wp.pl>
2025-09-07 01:33:16 +0200

    feat(ExtendPokemonCard): add basic state logic for sword & heart, redesign extend card layout

--- src/components/features/pokemon/ExtendPokemonCard.jsx
+++ src/components/features/pokemon/ExtendPokemonCard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import clsx from "clsx";
 import Wrapper from "../../shared/Wrapper";
 import Image from "../../shared/Image";
 import Text from "../../shared/Text";
@@ -9,11 +11,21 @@ import { Heart } from "lucide-react";
 import { Sword } from "lucide-react";
 
 const ExtendPokemonCard = () => {
+  const [favourite, setFavourite] = useState(false);
+  const [battle, setBattle] = useState(false);
   const { pokemon } = useParams();
   const { theme } = useTheme();
   const { log } = useAuth();
   const { pokemonsList } = usePokemonsArrayAPI();
   console.log(pokemon);
+
+  const baseClass = " w-12 h-12 font-black cursor-pointer";
+  const activeClass =
+    "text-red-700 font-black w-12 h-12 border-4 rounded-lg cursor-pointer";
+  const swordActiveClass = battle === true ? activeClass : "";
+  const heartActiveClass = favourite === true ? activeClass : "";
+  const swordClass = clsx(baseClass, swordActiveClass);
+  const heartClass = clsx(baseClass, heartActiveClass);
 
   if (!pokemonsList) return <p>Loading...</p>;
   const pokeData = pokemonsList.find((poke) => Number(pokemon) === poke.id);
@@ -23,7 +35,8 @@ const ExtendPokemonCard = () => {
 
   return (
     <Wrapper
-      className="flex gap-8 p-8 m-8 border-4 w-5xl rounded-4xl relative"
+      // key={}
+      className="flex gap-8 p-8 border-4 w-5xl rounded-4xl relative"
       variantBackground={theme}
     >
       <Wrapper className="border-4 rounded-4xl h-96 w-96" variant={theme}>
@@ -34,20 +47,36 @@ const ExtendPokemonCard = () => {
         />
       </Wrapper>
       <Wrapper className="flex flex-col justify-between">
-        <Wrapper className="flex absolute top-8 right-8" variantLog={log}>
+        <Wrapper variant={log}>
           <Text className="text-2xl font-bold text-green-700">WIN:</Text>
           <Text className="text-2xl font-bold text-red-700">LOSS:</Text>
-          <Sword />
-          <Heart />
+        </Wrapper>
+        <Wrapper className="flex absolute top-8 right-8 gap-2" variantLog={log}>
+          <Sword
+            onClick={() => setBattle((prev) => !prev)}
+            className={swordClass}
+          />
+          <Heart
+            onClick={() => setFavourite((prev) => !prev)}
+            className={heartClass}
+          />
         </Wrapper>
         <Wrapper>
-          <Text tag="h3" className="my-2 font-black uppercase">
+          <Text tag="h3" className="text-4xl mb-12 font-black uppercase text-center">
             {pokeData.name}
           </Text>
-          <Text strong={"HEIGHT: "}>{pokeData.height / 10} m</Text>
-          <Text strong={"WEIGHT: "}>{pokeData.weight / 10} kg</Text>
-          <Text strong={"BASE EXP: "}>{pokeData.base_experience}</Text>
-          <Text strong={"ABILITY: "}>{pokeData.abilities[0].ability.name}</Text>
+          <Wrapper className="flex gap-16">
+            <Wrapper className="flex flex-col gap-8">
+              <Text className="text-2xl" strong={"HEIGHT: "}>{pokeData.height / 10} m</Text>
+              <Text className="text-2xl" strong={"WEIGHT: "}>{pokeData.weight / 10} kg</Text>
+            </Wrapper>
+            <Wrapper className="flex flex-col gap-8">
+              <Text className="text-2xl" strong={"BASE EXP: "}>{pokeData.base_experience}</Text>
+              <Text className="text-2xl" strong={"ABILITY: "}>
+                {pokeData.abilities[0].ability.name}
+              </Text>
+            </Wrapper>
+          </Wrapper>
         </Wrapper>
       </Wrapper>
     </Wrapper>
