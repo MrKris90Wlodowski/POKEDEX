80cf984cd9c29a6fd1f7e2a10e2e627b641a1891
Kris1990_PL <MRK1990@wp.pl>
2025-09-11 22:01:05 +0200

    feat(useGetRecordPoke): create hook to fetch Pokemon by id

--- src/hooks/usePokemonImage.js
+++ src/hooks/usePokemonImage.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import POKE_API_URL from "../config/basePokeAPI";
 
 const usePokemonImage = () => {
-  const [pokemonImage, setPokemonImage] = useState([]);
+  const [pokemonsImage, setPokemonImage] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -11,25 +11,24 @@ const usePokemonImage = () => {
   useEffect(() => {
     setLoading(true);
     fetch(POKE_API_IMG)
-    .then((res) => res.json())
-    .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         return Promise.all(
-            data.result.map((poke) => fetch(poke.url).then((res) => res.json()))
-        )
-        .then(data => {
-            setPokemonImage(data);
-            setLoading(false);
-        }
-        )
-        .catch((error) => {
-            setLoading(false);
-            setError(error);
-            console.log(error);
-        })
-    });
-  },[]);
+          data.result.map((poke) => fetch(poke.url).then((res) => res.json()))
+        );
+      })
+      .then((data) => {
+        setPokemonImage(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError(error);
+        console.log(error);
+      });
+  }, []);
 
-  return { pokemonImage, loading, error };
+  return { pokemonsImage, loading, error };
 };
 
-export default usePokemonImage
+export default usePokemonImage;
