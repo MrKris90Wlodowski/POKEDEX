df457e150e934151c04f805724e0068b0f4d6a63
Kris1990_PL <MRK1990@wp.pl>
2025-09-15 04:36:14 +0200

    style(Arena): base design page

--- src/components/subpages/Arena.jsx
+++ src/components/subpages/Arena.jsx
@@ -1,14 +1,31 @@
 import Wrapper from "../shared/Wrapper";
 // import Text from "../shared/Text";
 import Button from "../shared/Button";
-import PokemonCard from "../features/pokemon/PokemonCard"
+import PokemonCard from "../features/pokemon/PokemonCard";
+import picturePokeball from "../../icons/pokeball.png";
+import { Flag } from "lucide-react";
 
 const Arena = () => {
+
   return (
-    <Wrapper>
-      <PokemonCard/>
-      <Button>BATTLE</Button>
-      <PokemonCard/>
+    <Wrapper className="flex justify-center items-center gap-8">
+      <PokemonCard
+        name="BLUE"
+        height="TOO SMALL"
+        exp="Rookie"
+        sourceImg={picturePokeball}
+        className="w-48 h-76"
+      />
+      <Button variant="default" className="text-[var(--yellow)] font-semibold text-2xl border-4 p-3 rounded-2xl w-40">
+        BATTLE
+      </Button>
+      <PokemonCard
+        name="RED"
+        height="TOO SMALL"
+        exp="Rookie"
+        sourceImg={picturePokeball}
+        className="w-48 h-76"
+      />
     </Wrapper>
   );
 };
