ca17d97aa278ad3a26bf83332834772469696f0f
Kris1990_PL <MRK1990@wp.pl>
2025-09-20 05:55:18 +0200

    fix(useCreatePoke): generate unique ID for every new pokemon

--- src/services/useCreatePoke.js
+++ src/services/useCreatePoke.js
@@ -5,21 +5,33 @@ const useCreatePoke = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
+
   const USER_API_URL = `${BASE_API_URL}/users`;
   const FAVOR_API_URL = `${BASE_API_URL}/pokemons`;
   const baseCreatePokeID = 151;
 
-  const getCounter = async (userID) => {
-    const res = await fetch(`${USER_API_URL}/${userID}`);
+  const getCounter = async (userData) => {
+    const res = await fetch(`${USER_API_URL}/${userData.id}`);
     if (!res.ok) throw new Error(`FETCH FAILED: ${res.status}`);
     const data = await res.json();
     return data.counterEditPoke ?? 0;
   };
 
+  const getAddCounter = async(userData) => {
+    const counter = await getCounter(userData)
+    const res = await fetch(`${USER_API_URL}/${userData.id}`,{
+      method: "PATCH",
+      headers: {"Content-type": "application/json"},
+      body: JSON.stringify({
+        counterEditPoke: counter + 1
+      })
+    });
+  }
+
   const createPoke = async (userData, dataForm) => {
     setLoading(true);
     try {
-      const counter = await getCounter(userData.id); 
+      const counter = await getCounter(userData); 
 
       await fetch(FAVOR_API_URL, {
         method: "POST",
@@ -38,7 +50,9 @@ const useCreatePoke = () => {
           lossBattle: null,
           isEdit: true,
         }),
-      });
+      })
+      const addConterPoke = await getAddCounter(userData);
+      ;
 
       // IN FUTURE (PUT/PATCH  /users/:id)
     } catch (err) {
